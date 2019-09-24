23a2cc38bb7e84954c8144e0e91817a87de4636f
tania <tanislavens@yahoo.com>
2019-09-24 18:28:48 -0400

    updated middleware to reflect template changes

--- middleware/validateTemplate.js
+++ middleware/validateTemplate.js
@@ -1,13 +1,13 @@
 function validateTemplate(req, res, next) {
     const { id: user_id } = req.params;
-    const { noun, verb, adjective, noun2, verb2, adjective2 } = req.body;
+    const { programming_language, noun, verb, ing_verb, ed_verb, noun2 } = req.body;
     if (!req.body) {
         return res.status(400).json({ error: "Body is required." });
     }
-    if (!noun && verb && adjective && noun2 && verb2 && adjective2) {
+    if (!programming_language && noun && verb && ing_verb && ed_verb && noun2) {
         return res.status(400).json({ error: "Please fill in all parts of speech." });
     }
-    req.body = { user_id, noun, verb, adjective, noun2, verb2, adjective2 };
+    req.body = { programming_language, noun, verb, ing_verb, ed_verb, noun2  };
     next();
 };
 
