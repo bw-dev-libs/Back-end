83ed744dc6161acd790b6e871e82979acf453e53
tania <tanislavens@yahoo.com>
2019-09-25 15:27:08 -0400

    adds pg support

--- knexfile.js
+++ knexfile.js
@@ -21,11 +21,8 @@ module.exports = {
     },
   },
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './database/auth.db3',
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
